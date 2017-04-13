c817d6c7fc14e1c93cc80e5c7a1ccafbb827cc52
RajKumar S <rajkumar.s@bridgesgi.com>
2017-04-13 19:14:57 +0530

    express js Added

--- src/components/match_items.jsx
+++ src/components/match_items.jsx
@@ -2,12 +2,13 @@ import React from 'react';
 
 const MatchItems = () => {
   return(
-  <li className="col-md-4 col-sm-6">
+    <div>
+  <li className="col-md-3 col-sm-4">
   <div className="text-center well well-sm"><span>Chennai Super Kings</span> <br/> VS <br/> <span>Mumbai Indians</span>
-  <hr></hr>
   <div>12 Apr 2016</div>
 </div>
   </li>
+</div>
   );
 }
 
