2289e3bfcca09de1f8c633b08383c5fa0dd476f7
RajKumar S <rajkumar.s@bridgesgi.com>
2017-04-13 16:43:15 +0530

    first Commit

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+import PageHeader from './components/header';
+class App extends Component {
+  render() {
+    return (  <PageHeader> </PageHeader>
+    )
+  }
+}
+
+export default App;
---
+++ src/components/header.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class PageHeader extends Component {
+
+  render(){
+    return(
+      <div>
+      <div className="text-center Header">Bridge Cricket</div>
+</div>
+    )
+  }
+}
+export default PageHeader;
